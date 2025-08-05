be6724e3fbf8939e440f6af910f4f0949822a90c
choi9970 <j.choi9970@gmail.com>
2025-08-05 17:51:09 +0900

    20250805

---
+++ day17/test2.js
@@ -0,0 +1,8 @@
+//돔 구축이 완료후
+document.addEventListener("DOMContentLoaded",function () {
+        const title= document.getElementById("title");
+        title.textContent="test";
+        
+});
+// dom을 포함된 모든 리소스 완료된후
+window.addEventListener("load",function () {});
